4c77bbb0e7706a952a81be12768d4e9ec21a639b
Thannojha <v.thanoojhap@gmail.com>
2022-06-26 17:08:46 -0500

    Commiting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-//checking
+//checking 5
 import LandingPage from './components/pages/LandingPage'
 import LoginPage from './components/pages/LoginPage'
 import RegisterPage from './components/pages/RegisterPage'
